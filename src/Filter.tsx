2782140be2e75197937e3a0af611723a1be7ef89
BlackHatMan <38476233+BlackHatMan@users.noreply.github.com>
2022-12-12 22:01:08 +0300

    feat: filter component

---
+++ src/Filter.tsx
@@ -0,0 +1,42 @@
+import { InputBase } from '@mui/material';
+import { Column } from '@tanstack/react-table';
+import { Table } from '@tanstack/react-table';
+
+interface propsFiler {
+  column: Column<any, any>;
+  table: Table<any>;
+}
+
+export function Filter({ column, table }: propsFiler) {
+  const typeValue = typeof table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id);
+  const columnFilterValue = column.getFilterValue();
+
+  return typeValue === 'number' ? (
+    <div>
+      <InputBase
+        type="number"
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])
+        }
+        placeholder={`Min`}
+      />
+      <InputBase
+        type="number"
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])
+        }
+        placeholder={`Max`}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </div>
+  ) : (
+    <InputBase
+      value={(columnFilterValue ?? '') as string}
+      onChange={(e) => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      inputProps={{ 'aria-label': 'search' }}
+    />
+  );
+}
--- src/TableMui.tsx
+++ src/TableMui.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,99 +6,35 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 
 import {
-  Column,
-  Table as ReactTable,
   useReactTable,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
-  ColumnDef,
   flexRender,
 } from '@tanstack/react-table';
 
-import { makeData, Response } from './fakeData';
+import { Filter } from './Filter';
+import { columns } from './scheme';
+import { makeData } from './fakeData';
 
 export const TableMui = () => {
-  const columns = useMemo<ColumnDef<Response>[]>(
-    () => [
-      {
-        header: 'smart table',
-        footer: (props) => props.column.id,
-        columns: [
-          {
-            accessorFn: (row) => row.name,
-            id: 'name',
-            cell: (info) => info.getValue(),
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.sum,
-            id: 'sum',
-            cell: (info) => info.getValue(),
-            header: () => 'sum',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.currency,
-            id: 'currency',
-            header: () => 'currency',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.qty,
-            id: 'qty',
-            header: () => 'quantity',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.volume,
-            id: 'volume',
-            header: () => 'volume',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.status,
-            id: 'status',
-            header: () => 'status',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.delivery_date,
-            id: 'delivery_date',
-            header: () => 'delivery date',
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-    ],
-    []
-  );
-
   const [data] = useState(() => makeData(10));
-
-  return <LocalTable {...{ data, columns }} />;
-};
-
-function LocalTable({ data, columns }: { data: Response[]; columns: ColumnDef<Response>[] }) {
   const table = useReactTable({
     data,
     columns,
-    // Pipeline
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-    //
     debugTable: true,
   });
 
   return (
     <Box sx={{ width: '100%' }}>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ minWidth: 650 }} aria-label="table">
           <TableHead>
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
@@ -140,38 +76,4 @@ function LocalTable({ data, columns }: { data: Response[]; columns: ColumnDef<Re
       </TableContainer>
     </Box>
   );
-}
-function Filter({ column, table }: { column: Column<any, any>; table: ReactTable<any> }) {
-  const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id);
-
-  const columnFilterValue = column.getFilterValue();
-
-  return typeof firstValue === 'number' ? (
-    <div>
-      <InputBase
-        type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ''}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])
-        }
-        placeholder={`Min`}
-      />
-      <InputBase
-        type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ''}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])
-        }
-        placeholder={`Max`}
-        inputProps={{ 'aria-label': 'search' }}
-      />
-    </div>
-  ) : (
-    <InputBase
-      value={(columnFilterValue ?? '') as string}
-      onChange={(e) => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      inputProps={{ 'aria-label': 'search' }}
-    />
-  );
-}
+};
